530e415f2a55e9ee5f45354981aa7bd56558ea59
Ribao Wei <ribao.w@gmail.com>
2012-07-20 18:27:58 -0400

    add codde

---
+++ app.js
@@ -0,0 +1,72 @@
+var request = require("request"),
+	xml2js = require('xml2js'),
+	parser = new xml2js.Parser(),
+	express = require('express'),
+	app = express.createServer();
+
+var defaultSource =  ["http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050101T000000Z,20050102T000000Z/Magnitude,BGSEc?format=text",
+"http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050102T000000Z,20050103T000000Z/Magnitude,BGSEc?format=text",
+"http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050103T000000Z,20050104T000000Z/Magnitude,BGSEc?format=text"
+// "http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050104T000000Z,20050105T000000Z/Magnitude,BGSEc?format=text",
+// "http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050105T000000Z,20050106T000000Z/Magnitude,BGSEc?format=text",
+// "http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050106T000000Z,20050107T000000Z/Magnitude,BGSEc?format=text",
+// "http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050107T000000Z,20050108T000000Z/Magnitude,BGSEc?format=text",
+// "http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050108T000000Z,20050109T000000Z/Magnitude,BGSEc?format=text",
+// "http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050109T000000Z,20050110T000000Z/Magnitude,BGSEc?format=text",
+// "http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050110T000000Z,20050111T000000Z/Magnitude,BGSEc?format=text",
+// "http://cdaweb.gsfc.nasa.gov/WS/cdasr/1/dataviews/sp_phys/datasets/AC_H1_MFI/data/20050111T000000Z,20050112T000000Z/Magnitude,BGSEc?format=text"
+];
+
+function parseSource(source){
+	return source.trim().split("\n");
+}
+
+function process(source, callback, res){
+	var result = [];
+	source.forEach(function(url){
+		processUrl(url, function(){
+			console.log(result);
+			if (result.length === source.length){
+				callback(result,res);
+			}
+		});
+	})
+
+	function processUrl(url, callback){
+		request.get({uri: url}, function(error, response, body) {
+			var start = +new Date();
+		    if (response.statusCode !== 200) {
+		    	console.log(url);
+		        console.log(response.statusCode);
+		    } else {
+		    	parser.parseString(body, function(err, res){
+		    		var url2 = res.FileDescription.Name;
+		    		request.get({uri:url2}, function(error, response, body){
+		    			var end = +new Date();
+		    			result.push({"url" : url, "time" : (end - start)});
+		    			callback();
+		    		})
+		    	});
+		    }
+		});
+	}
+}
+
+// uses res, req
+function sendResult(result, res){
+	res.contentType("text");
+	res.send(result.map(function(d){
+		return "URL: "+d.url+"\n"+"time: "+d.time + "ms";
+	}).join('\n\n\n'));
+}
+
+app.use(express.bodyParser());
+
+app.get('/', function(req, res){
+	process(defaultSource, sendResult, res);
+})
+app.post('/', function(req, res){
+	process(parseSource(req.body.source), sendResult, res);
+})
+
+app.listen(8000);
