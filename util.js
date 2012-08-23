a7ddf4bcf3f14ece8e63660002a13c3fa92b4922
Ribao Wei <ribao.w@gmail.com>
2012-08-23 16:52:18 -0400

    correct require moment

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 var fs = require("fs"),
 	crypto = require("crypto"),
 	moment = require("prettydate"),
-	request = require("request");
+	request = require("moment");
 
 var logger = require("./logger.js");
 
