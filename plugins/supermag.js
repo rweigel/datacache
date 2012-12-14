0671dafd60b7ebecb4c96bd2ff19c70a11b7aa0a
Bob Weigel <rweigel@gmu.edu>
2012-12-14 14:54:10 -0500

    Change matched URL

--- plugins/supermag.js
+++ plugins/supermag.js
@@ -1,15 +1,20 @@
 exports.name = "supermag";
 
 exports.match = function(url){
-	return url.split("/")[2].toLowerCase()==="supermag.uib.no";
+	return url.split("/")[2].toLowerCase()==="supermag.jhuapl.edu";
 }
 
 exports.extractData = function(data){
-	var re = /^([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)$|^([a-zA-Z]+)\s+([\d-]+)\s([\d-]+)\s([\d-]+)$/;
-	return data.toString()
+    // Time stamp line
+    var re1 = /^([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)/;
+    // Data line
+    var re2 = /\n([a-zA-Z]+)\s+([\d-]+)\s([\d-]+)\s([\d-]+)/g;
+    // First combine timestamp and data line and remove station string.  Then spit on newlines and keep data lines.
+    return data.toString()
+	                .replace(re2," $2 $3 $4")
 			.split("\n")
 			.filter(function(line){
-				return line.search(re)!=-1;
+				return line.search(re1)!=-1;
 			})
-			.join("\n");;
+			.join("\n");
 };
