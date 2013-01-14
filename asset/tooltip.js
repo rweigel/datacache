8aa3fa9806e746731b0043f150c62d5b19d3ee47
Bob Weigel <rweigel@gmu.edu>
2013-01-14 08:38:22 -0500

    Turn off when hovered over

--- asset/tooltip.js
+++ asset/tooltip.js
@@ -2,13 +2,16 @@ function tooltip(el) {
 
 	$(el).hover(
 		function () {
-			$(this).children(".tooltip").remove();
-			$(this).attr('tooltip',$(this).attr('title') + ($(this).attr('tooltip')||""));
-			$(this).attr('title','');
-			var str = $(this).attr("title") + ($(this).attr("tooltip")||"");
-			$(this).append("<span class='tooltip'>"+str+"</span>");
-			var offset = $(this).offset();
-			$(this).children(".tooltip").offset({ top: offset.top-25, left: offset.left});
+				$(this).children(".tooltip").eq(0).remove();
+				$(this).attr('tooltip',($(this).attr('title') || "") + ($(this).attr('tooltip')||""));
+				$(this).attr('title','');
+				var str = $(this).attr("title") + ($(this).attr("tooltip") || "");
+				$(this).append("<span class='tooltip'>"+str.replace(/ /g,'&nbsp;')+"</span>");
+				if (str === "")
+					$(this).children(".tooltip").css('display','none');
+				var offset = $(this).offset();
+				$(this).children(".tooltip").eq(0).offset({ top: offset.top+15, left: offset.left});
+				$(this).children(".tooltip").eq(0).mouseover(function () {$(this).hide()});
 		},
 		function () {												
 			$(this).children(".tooltip").css('display','none');
