e902056e9427634072c031bc0017d337149692fd
Bob Weigel <rweigel@gmu.edu>
2013-02-04 08:40:15 -0500

    Added memory option to start-up, new preset.

--- datacache.sh
+++ datacache.sh
@@ -2,8 +2,8 @@
 #
 # Startup script for the DataCache server
 #
-
 PORT=8000
+MEMORY=--max-old-space-size=1900
 APP=/usr/local/datacache/app.js
 NODE=/usr/bin/nodejs
 LOG=/var/log/datacache
@@ -11,21 +11,21 @@ LOG=/var/log/datacache
 mkdir -p $LOG
 
 case $1 in
-	start)
-	        sudo -u www-data $NODE $APP $PORT -id 1 >> $LOG/datacache.log 2>&1 &
-	;;
-	stop)
-	        pid=`pgrep -f "$NODE $APP $PORT -id 1"`
-	        sudo kill -9 $pid
-	;;
-	restart)
-	        pid=`pgrep -f "$NODE $APP $PORT -id 1"`
-	        sudo kill -9 $pid
-	        /etc/init.d/datacache
-	;;
-	*)
-		echo "Usage: /etc/init.d/datacache start|stop|restart"
-	;;
+        start)
+                sudo -u www-data $NODE $MEMORY $APP $PORT -id 1 >> $LOG/datacache.log 2>&1 &
+        ;;
+        stop)
+                pid=`pgrep -f "$NODE $MEMORY $APP $PORT -id 1"`
+                sudo kill -9 $pid
+        ;;
+        restart)
+                pid=`pgrep -f "$NODE $MEMORY $APP $PORT -id 1"`
+                sudo kill -9 $pid
+                /etc/init.d/datacache
+        ;;
+        *)
+                echo "Usage: /etc/init.d/datacache start|stop|restart"
+        ;;
 esac
 
 exit 0
