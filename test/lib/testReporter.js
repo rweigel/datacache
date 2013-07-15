8d0cd6deb026ec8091c5a9cc6c6ae7562c95395c
awesomejay <jay.jieyi.long@gmail.com>
2013-07-14 18:45:37 -0700

    Improved test reporter. Now it ignores newlines in the template but instead translates "\n" into newline. Now it is easier to maintain text format in the template

--- test/lib/testReporter.js
+++ test/lib/testReporter.js
@@ -4,7 +4,10 @@ var fs = require("fs"),
 var simpleTemplate = fs.readFileSync(__dirname + "/simple.template").toString("utf8");
 
 function simpleReporter(results){
-	return handlebars.compile(simpleTemplate)(results);
+	return handlebars
+			.compile(simpleTemplate)(results)
+			.replace(/\r\n|\r|\n/g, "")
+			.replace(/\\n/g, "\n");
 }
 
 exports.simpleReporter = simpleReporter;
