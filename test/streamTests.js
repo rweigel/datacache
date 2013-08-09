b5f3b6d300580d69407b716de54251801be7b2a2
Bob Weigel <rweigel@gmu.edu>
2013-08-08 21:12:00 -0400

    Demo of problem with requesting gzipped content.

--- test/streamTests.js
+++ test/streamTests.js
@@ -1,6 +1,17 @@
 var server = process.argv[2] || "http://localhost:8000/";
 //var server = process.argv[2] || "http://datacache.org/dc/";
 
+var fs      = require("fs");
+var jsdiff  = require("diff");
+var async   = require("async");
+var logger  = require("./lib/logger")();
+var request = require("request");
+var md5     = require("./lib/util").md5;
+var zlib = require('zlib');
+
+var runner    = require("./lib/testRunner")();
+var suite     = runner.suite;
+var assertNot = runner.assertNot;
 
 // Testing streamOrder, streamFilterReadLines, and streamFilterReadBytes 
 
@@ -36,12 +47,11 @@ tests[j].sort = false;
 tests[j].n    = 10;
 tests[j].md5  = "";
 
-// Fails
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=false&streamFilterReadBytes=720&prefix="+prefix+"&source="+source;
 tests[j].sort = true;
-tests[j].n    = 2;
+tests[j].n    = 10;
 tests[j].md5  = "";
 
 // Base test.
@@ -69,7 +79,6 @@ tests[j].sort = false;
 tests[j].n    = 10;
 tests[j].md5  = "";
 
-// Fails
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=false&streamFilterReadBytes=720&prefix="+prefix+"&source="+source;
@@ -77,24 +86,72 @@ tests[j].sort = true;
 tests[j].n    = 10;
 tests[j].md5  = "";
 
+j = j+1;
+tests[j]      = {};
+tests[j].url  = args + "&streamOrder=true&streamFilterReadLines=10&streamFilter=replace('2013','2013')&prefix="+prefix+"&source="+source;
+tests[j].sort = false;
+tests[j].n    = 10;
+tests[j].md5  = "";
 
-var fs      = require("fs");
-var jsdiff  = require("diff");
-var async   = require("async");
-var logger  = require("./lib/logger")();
-var request = require("request");
-var md5     = require("./lib/util").md5;
+j = j+1;
+tests[j]      = {};
+tests[j].url  = args + "&streamOrder=false&streamFilterReadBytes=720&streamFilter=replace('2013','2013')&prefix="+prefix+"&source="+source;
+tests[j].sort = true;
+tests[j].n    = 10;
+tests[j].md5  = "";
 
-var runner    = require("./lib/testRunner")();
-var suite     = runner.suite;
-var assertNot = runner.assertNot;
+var source    = "03vmin.min";
 
+j = j+1;
+tests[j]      = {};
+tests[j].url  = args + "&streamOrder=true&streamFilterReadBytes=720&streamGzip=true&prefix="+prefix+"&source="+source;
+tests[j].gunzip = true;
+tests[j].sort = true;
+tests[j].n    = 1;
+tests[j].md5  = "";
+
+if (0) {
+	// Why is a.out.gz and b.out.gz different from c.out.gz created with curl?
+	// (curl gives the correct result).
+	// curl -g "http://localhost:8000/sync?return=stream&forceUpdate=true&forceWrite=true&lineRegExp=^[0-9]&streamOrder=true&streamFilterReadBytes=720&streamGzip=true&prefix=http://localhost:8000/test/data-stream/bou201308&source=03vmin.min" > c.out.gz
+
+	var http = require('http');
+	
+	var options = {
+	    host: 'localhost',
+	    path: '/sync?return=stream&forceUpdate=true&forceWrite=true&lineRegExp=^[0-9]&streamOrder=true&streamFilterReadBytes=720&streamGzip=true&prefix=http://localhost:8000/test/data-stream/bou201308&source=03vmin.min',
+	    port: 8000
+	    
+	}
+	var request = http.request(options, function (res) {
+	    var data = '';
+	    res.on('data', function (chunk) {
+	        data += chunk;
+	    });
+	    res.on('end', function () {
+			fs.writeFileSync("a.out.gz",data);
+	        //console.log(data);
+	    });
+	});
+	request.on('error', function (e) {
+	    console.log(e.message);
+	});
+	request.end();
+	
+	var request = require('request');
+	request(tests[j].url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    fs.writeFileSync("b.out.gz",body);
+	  }
+	})
+	return;
+}
 logger.i("streamTests.js tests started.");
 
 var settings = {};
 
 //runtest(0);
-runtest(6);
+runtest(10);
 var debug = false;
 
 function runtest(i) {
@@ -135,6 +192,7 @@ function testreport(err) {
 			if (debug) console.log(settings);
 
 			if (!fs.existsSync("data-stream/streamTests.out."+settings.i)) {
+				console.log(result.info.body.length)
 				if (debug) console.log("Writing "+"data-stream/streamTests.out."+settings.i);
 				fs.writeFileSync("data-stream/streamTests.out."+settings.i,result.info.body);
 				if (debug) console.log("Writing "+"data-stream/streamTests.md5."+settings.i);
@@ -142,6 +200,9 @@ function testreport(err) {
 			} else {
 				if (debug) console.log("Reading "+"data-stream/streamTests.md5."+settings.i);
 				var expectedmd5 = fs.readFileSync("data-stream/streamTests.md5."+settings.i).toString();
+				//zlib.createGunzip();
+				//zlib.gunzip(result.info.body, function (err, buffer) {console.log(buffer);})
+				//console.log(zlib.createGunzip().pipe(result.info.body));
 				if (settings.sort) {
 					if (debug) {
 						console.log("Sorting");
