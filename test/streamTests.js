badf2438c82a24182bb75ea499783efae121223f
Bob Weigel <rweigel@gmu.edu>
2013-08-10 21:46:43 -0400

    All of these tests run without error.  Execute from command line as node streamTests.js A B, where A = 1, 2, …, 10 (test number) and B = true (async) or false.

--- test/streamTests.js
+++ test/streamTests.js
@@ -1,5 +1,5 @@
-var server = process.argv[2] || "http://localhost:8000/";
-//var server = process.argv[2] || "http://datacache.org/dc/";
+var server = process.argv[4] || "http://localhost:8000/";
+//var server = process.argv[4] || "http://datacache.org/dc/";
 
 var fs      = require("fs");
 var jsdiff  = require("diff");
@@ -7,7 +7,7 @@ var async   = require("async");
 var logger  = require("./lib/logger")();
 var request = require("request");
 var md5     = require("./lib/util").md5;
-var zlib = require('zlib');
+var zlib    = require('zlib');
 
 var runner    = require("./lib/testRunner")();
 var suite     = runner.suite;
@@ -18,7 +18,7 @@ var assertNot = runner.assertNot;
 //var prefix    = "http://magweb.cr.usgs.gov/data-stream/magnetometer/BOU/OneMinute/bou201308";
 var prefix    = server + "test/data-stream/bou201308";
 var args      = server + "sync?return=stream&forceUpdate=true&forceWrite=true&lineRegExp=^[0-9]";
-
+//var args      = server + "sync?return=stream&lineRegExp=^[0-9]";
 // Base test.
 
 // These two files are served without delay
@@ -28,87 +28,135 @@ var tests     = [];
 var j = 0;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamFilterReadLines=10&prefix="+prefix+"&source="+source;
-tests[j].sort = false;
-tests[j].n    = 10;
-tests[j].md5  = "";
+tests[j].n    = 50;
+tests[j].md5  = "651f75d088a29b4a0a95e97a1bcccd48";
 
 // Stream order = false.  Sorted stream should have same md5 as previous.
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=false&streamFilterReadLines=10&prefix="+prefix+"&source="+source;
-tests[j].sort = true;
-tests[j].n    = 10;
-tests[j].md5  = "";
+tests[j].n    = 50;
+tests[j].md5  = "651f75d088a29b4a0a95e97a1bcccd48";
 
+// The following two should be the same as the above, but the line reader
+// does not preserve the newline character - it replaces it with \n.
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamFilterReadBytes=720&prefix="+prefix+"&source="+source;
 tests[j].sort = false;
-tests[j].n    = 10;
-tests[j].md5  = "";
+tests[j].n    = 50;
+tests[j].md5  = "8ee474572d7118be2a842fb140337cd9";
 
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=false&streamFilterReadBytes=720&prefix="+prefix+"&source="+source;
 tests[j].sort = true;
-tests[j].n    = 10;
-tests[j].md5  = "";
+tests[j].n    = 50;
+tests[j].md5  = "8ee474572d7118be2a842fb140337cd9";
 
 // Base test.
 // These three files are served with random delay between 0 and 100 ms.
 var source    = "03vmin.min%0A04vmin.min%0A05vmin.min";
 
+// Not working reliably.
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamFilterReadLines=10&prefix="+prefix+"&source="+source;
-tests[j].sort = false;
-tests[j].n    = 10;
-tests[j].md5  = "";
+tests[j].n    = 50;
+tests[j].md5  = "cef9e50efae894c42293ed5922af2b8e";
 
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=false&streamFilterReadLines=10&prefix="+prefix+"&source="+source;
-tests[j].sort = true;
-tests[j].n    = 10;
-tests[j].md5  = "";
+tests[j].n    = 50;
+tests[j].md5  = "cef9e50efae894c42293ed5922af2b8e";
 
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamFilterReadBytes=720&prefix="+prefix+"&source="+source;
-tests[j].sort = false;
-tests[j].n    = 10;
-tests[j].md5  = "";
+tests[j].n    = 50;
+tests[j].md5  = "4dcc638a9d40bdf4f4c1c6d38ecfa979";
 
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=false&streamFilterReadBytes=720&prefix="+prefix+"&source="+source;
 tests[j].sort = true;
-tests[j].n    = 10;
-tests[j].md5  = "";
+tests[j].n    = 50;
+tests[j].md5  = "4dcc638a9d40bdf4f4c1c6d38ecfa979";
 
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamFilterReadLines=10&streamFilter=replace('2013','2013')&prefix="+prefix+"&source="+source;
-tests[j].sort = false;
-tests[j].n    = 10;
-tests[j].md5  = "";
+tests[j].n    = 50;
+tests[j].md5  = "cef9e50efae894c42293ed5922af2b8e";
 
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=false&streamFilterReadBytes=720&streamFilter=replace('2013','2013')&prefix="+prefix+"&source="+source;
-tests[j].sort = true;
-tests[j].n    = 10;
-tests[j].md5  = "";
-
-var source    = "03vmin.min";
+tests[j].n    = 50;
+tests[j].md5  = "4dcc638a9d40bdf4f4c1c6d38ecfa979";
 
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamFilterReadBytes=720&streamGzip=true&prefix="+prefix+"&source="+source;
-tests[j].gunzip = true;
-tests[j].sort = true;
-tests[j].n    = 1;
-tests[j].md5  = "";
+tests[j].n    = 10;
+tests[j].md5  = "4dcc638a9d40bdf4f4c1c6d38ecfa979";
+
+j = parseInt(process.argv[2]) || 0;
+
+if (process.argv[3]) { 
+	var async = s2b(process.argv[3]);
+} else {
+	var async = false;
+}
+
+function s2b(str) {if (str === "true") {return true} else {return false}}
+
+if (1) {
+		var child, sys = require('sys'), exec = require('child_process').exec;
+
+		if (!async) {
+			checkmd5(0,true);
+		} else {
+			for (k = 0;k < tests[j].n;k++) {
+				checkmd5(k,false);
+			}
+		}		
+		return;
+}
+function command(k) {
+		var fname = "data-stream/out." + j + "." + k;
+		if (tests[j].url.match("streamGzip=true")) {
+			var com = 'curl -s -g "' + tests[j].url + '" | gunzip | tee ' + fname;
+		} else {
+			var com = 'curl -s -g "' + tests[j].url + '" | tee ' + fname;
+		}
+		if (tests[j].url.match("streamOrder=false")) {
+			com = com + " | sort";
+		}
+		com = com + " | md5";
+		if (k == 0) 
+			console.log(com);		
+		return com;
+}
+function checkmd5(k,sync) {
+			child = exec(command(k), function (error, stdout, stderr) {
+				console.log(k + " " + stdout.replace(/\n$/,""));
+				if (tests[j].md5 !== stdout.replace(/\n$/,"")) {
+					console.log("Error")
+					//diff(fname,"data-stream/out." + j + ".0");
+				}
+				if (sync == true && k < tests[j].n-1) {					
+					checkmd5(k+1,true);
+				}
+			});
+}
+function diff(f1,f2) {
+		child = exec('diff ' + f1 + ' ' + f2, function (error, stdout, stderr) {
+			console.log("difference between " + f1 + " and " + f2 + ": ");
+			console.log(stdout);
+		});
+}
 
 if (0) {
 	// Why is a.out.gz and b.out.gz different from c.out.gz created with curl?
@@ -119,9 +167,9 @@ if (0) {
 	
 	var options = {
 	    host: 'localhost',
-	    path: '/sync?return=stream&forceUpdate=true&forceWrite=true&lineRegExp=^[0-9]&streamOrder=true&streamFilterReadBytes=720&streamGzip=true&prefix=http://localhost:8000/test/data-stream/bou201308&source=03vmin.min',
-	    port: 8000
-	    
+	    path: '/sync?return=stream&forceUpdate=true&forceWrite=true&lineRegExp=^[0-9]&streamOrder=true&streamFilterReadBytes=720&streamGzip=true&prefix=http://localhost:8000/test/data-stream/bou201308&source=03vmin.min%0A04vmin.min',
+	    port: 8000,
+	    encoding: null
 	}
 	var request = http.request(options, function (res) {
 	    var data = '';
@@ -129,6 +177,7 @@ if (0) {
 	        data += chunk;
 	    });
 	    res.on('end', function () {
+	    		console.log(data.length);
 			fs.writeFileSync("a.out.gz",data);
 	        //console.log(data);
 	    });
@@ -137,11 +186,24 @@ if (0) {
 	    console.log(e.message);
 	});
 	request.end();
-	
+
 	var request = require('request');
-	request(tests[j].url, function (error, response, body) {
+	request({url: tests[j].url, encoding: null}, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
+	  	console.log(body.length);
 	    fs.writeFileSync("b.out.gz",body);
+		zlib.gunzip(body, function (err, buffer) {
+			console.log('node.js gunzip');
+			console.log(buffer.toString());
+		});
+	    // http://nodejs.org/api.html#_child_processes
+		var sys = require('sys')
+		var exec = require('child_process').exec;
+		var child;		
+		child = exec("gunzip -c b.out.gz", function (error, stdout, stderr) {
+		  console.log('command line gunzip: ');
+		  console.log(stdout);
+		});
 	  }
 	})
 	return;
@@ -151,7 +213,7 @@ logger.i("streamTests.js tests started.");
 var settings = {};
 
 //runtest(0);
-runtest(10);
+runtest(9);
 var debug = false;
 
 function runtest(i) {
@@ -169,11 +231,13 @@ function testrun(n, next) {
 		logger.i("test #" + n);
 		
 		request({
-			uri: settings.url
+			uri: settings.url,
+			encoding: null
 		}, function(err, res, body){
 			// Add info to runner.results
 			testInfo("err", err);
 			testInfo("res", res);
+			console.log(body.length);
 			testInfo("body", body);
 			suiteDone();
 			next();
@@ -189,63 +253,98 @@ function testreport(err) {
 		.suites
 		.map(function(result){
 
+			//console.log(result.info.body.length);
 			if (debug) console.log(settings);
-
 			if (!fs.existsSync("data-stream/streamTests.out."+settings.i)) {
-				console.log(result.info.body.length)
-				if (debug) console.log("Writing "+"data-stream/streamTests.out."+settings.i);
-				fs.writeFileSync("data-stream/streamTests.out."+settings.i,result.info.body);
-				if (debug) console.log("Writing "+"data-stream/streamTests.md5."+settings.i);
-				fs.writeFileSync("data-stream/streamTests.md5."+settings.i,md5(result.info.body));
+				if (settings.gunzip) {
+
+					// This does not work when two gzipped files are concatenated.  It
+					// only returns first gzip file.  Even if it did work, we would need
+					// a sync version of this.
+					//zlib.gunzip(result.info.body, function (err, buffer) {
+					//	resultmd5 = write(buffer.toString());
+					//});
+					
+				} else {
+					resultmd5 = write(result.info.body);
+				}
+				function write(data) {
+					if (debug) console.log("Writing "+"data-stream/streamTests.out."+settings.i);
+					fs.writeFileSync("data-stream/streamTests.out."+settings.i,data,'binary');					
+					resultmd5 = md5(data);
+					if (debug) console.log("Writing "+"data-stream/streamTests.md5."+settings.i);
+					fs.writeFileSync("data-stream/streamTests.md5."+settings.i,resultmd5,'binary');
+					console.log(fs.readFileSync("data-stream/streamTests.md5."+settings.i).toString());
+				}
 			} else {
 				if (debug) console.log("Reading "+"data-stream/streamTests.md5."+settings.i);
-				var expectedmd5 = fs.readFileSync("data-stream/streamTests.md5."+settings.i).toString();
-				//zlib.createGunzip();
-				//zlib.gunzip(result.info.body, function (err, buffer) {console.log(buffer);})
-				//console.log(zlib.createGunzip().pipe(result.info.body));
-				if (settings.sort) {
-					if (debug) {
-						console.log("Sorting");
-						console.log("Before sort")
-						console.log(result.info.body);
-						console.log("After sort")
-					}
-					if (result.info.body.match(/\n$/)) {
-						if (debug) {
-							console.log("File has trailing newline.");
-							console.log(result.info.body.replace(/\n$/,"").split("\n").sort().join("\n")+"\n");
-						}
-						result.info.body = result.info.body.replace(/\n$/,"").split("\n").sort().join("\n")+"\n";
-					} else {
-						if (debug) { 
-							console.log(result.info.body.split("\n").sort().join("\n"));
-						}
-						result.info.body = result.info.body.split("\n").sort().join("\n");
-					}
+				var expectedmd5 = fs.readFileSync("data-stream/streamTests.md5."+settings.i,'binary').toString();
+				console.log(fs.readFileSync("data-stream/streamTests.md5."+settings.i).toString());
+				if (settings.gunzip) {
+
+					//zlib.gunzip(result.info.body, function (err, buffer) {
+					//	resultmd5 = checkresults(buffer.toString);
+					//});
+
 				} else {
-					if (debug) {
-						console.log(result.info.body);
-					}
+					resultmd5 = checkresults(result.info.body.toString());
 				}
-					  
-				var resultmd5   = md5(result.info.body);
-				if (expectedmd5 !== resultmd5) {
-					console.log("Test failed.");
-					console.log("--- Expected");
-					console.log(fs.readFileSync("data-stream/streamTests.out."+settings.i).toString());
-					console.log("--- Received");
-					console.log(result.info.body);
-					console.log("---");
-					//var diffs = jsdiff.diffLines(fs.readFileSync("data-stream/streamTests.out."+settings.i).toString(), result.info.body);
-					//console.log(diffs.added);
-					//console.log(diffs.removed);
-				} 
+				
+				function checkresults(data) {
+					if (settings.sort) {
+						//if (debug) {
+							console.log("Sorting");
+							console.log("Before sort")
+							console.log(data);
+							console.log("After sort")
+						//}
+						if (data.match(/\n$/)) {
+							//if (debug) {
+								console.log("File has trailing newline.");
+								console.log(data.replace(/\n$/,"").split("\n").sort().join("\n")+"\n");
+							//}
+							data = data.replace(/\n$/,"").split("\n").sort().join("\n")+"\n";
+						} else {
+							//if (debug) { 
+								console.log(data.split("\n").sort().join("\n"));
+							//}
+							data = data.split("\n").sort().join("\n");
+						}
+					} else {
+						if (debug) {
+							console.log(data);
+						}
+					}
+						  
+					resultmd5 = md5(data);
+					console.log("expected md5: " + expectedmd5);
+					console.log("received md5: " + resultmd5);
+					if (expectedmd5 !== resultmd5) {
+						console.log("Test failed.  Showing diff.");
+						//console.log("--- Expected");
+						//console.log(fs.readFileSync("data-stream/streamTests.out."+settings.i).toString());
+						//console.log("--- Received");
+						//console.log(data);
+						//console.log("---");
+						var fname = "/tmp/"+Math.random().toString().substring(1);
+						console.log(fname);
+						fs.writeFileSync(fname,data,'binary');
+						var child, sys = require('sys'), exec = require('child_process').exec;
+						
+						child = exec("diff " + fname + " " + "data-stream/streamTests.out."+settings.i, function (error, stdout, stderr) {
+			  				console.log('diff: ');
+			  				console.log(stdout);
+						});
+						
+						//var diffs = jsdiff.diffLines(fs.readFileSync("data-stream/streamTests.out."+settings.i).toString(), result.info.body);
+						//console.log(diffs.added);
+						//console.log(diffs.removed);
+					} 
+				}
 			}
-			return resultmd5;
 		})
-		.join("\n");
 	
-	logger.i("\n" + testReport);
+	//logger.i("\n" + testReport);
 	logger.i("streamTests.js tests ended.");
 
 	// Does not work.
