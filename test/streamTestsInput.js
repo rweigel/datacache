917d4face1f8b21bdd97912e7f69d1182fea61d5
Bob Weigel <rweigel@gmu.edu>
2013-08-12 01:18:23 -0400

    Updated md5s to reflect new line reader that keeps \r\n

--- test/streamTestsInput.js
+++ test/streamTestsInput.js
@@ -1,16 +1,6 @@
 // Testing streamOrder, streamFilterReadLines, and streamFilterReadBytes 
 
 ////////////////////////////////////////////////////////////////////////////
-// Real server
-var n = 2;
-var prefix    = "http://magweb.cr.usgs.gov/data/magnetometer/BOU/OneMinute/bou201308";
-
-// Simulated server
-// First two files are served without delay from 
-// Next three files are served with random delay between 0 and 100 ms.
-var n = 50;
-var prefix    = server + "/test/data-stream/bou201308";
-
 // Simulated server
 // First two files are served without delay from 
 // Next three files are served with random delay between 0 and 100 ms.
@@ -20,8 +10,19 @@ var prefix    = "http://datacache.org/dc/test/data-stream/bou201308";
 // Mirror of real server.
 var n = 50;
 var prefix    = "http://mag.gmu.edu/tmp/magweb.cr.usgs.gov/data/magnetometer/BOU/OneMinute/bou201308";
-////////////////////////////////////////////////////////////////////////////
 
+
+// Real server
+var n = 10;
+var prefix    = "http://magweb.cr.usgs.gov/data/magnetometer/BOU/OneMinute/bou201308";
+
+// Simulated server
+// First two files are served without delay from 
+// Next three files are served with random delay between 0 and 100 ms.
+var n = 20;
+var prefix    = server + "test/data-stream/bou201308";
+
+////////////////////////////////////////////////////////////////////////////
 
 var args      = server + "sync?return=stream&forceUpdate=true&forceWrite=true&lineRegExp=^[0-9]";
 
@@ -32,20 +33,20 @@ var j = 0;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamFilterReadLines=10&prefix="+prefix+"&source="+source;
 tests[j].n    = n || 50;
-tests[j].md5  = "651f75d088a29b4a0a95e97a1bcccd48";
+tests[j].md5  = "8ee474572d7118be2a842fb140337cd9";
 
 var j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamGzip=true&streamFilterReadLines=10&prefix="+prefix+"&source="+source;
 tests[j].n    = n || 50;
-tests[j].md5  = "651f75d088a29b4a0a95e97a1bcccd48";
+tests[j].md5  = "8ee474572d7118be2a842fb140337cd9";
 
 // Stream order = false.  Sorted stream should have same md5 as previous.
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=false&streamFilterReadLines=10&prefix="+prefix+"&source="+source;
 tests[j].n    = n || 50;
-tests[j].md5  = "651f75d088a29b4a0a95e97a1bcccd48";
+tests[j].md5  = "8ee474572d7118be2a842fb140337cd9";
 
 // The following two should be the same as the above, but the line reader
 // does not preserve the newline character - it replaces it with \n.
@@ -70,13 +71,13 @@ j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamFilterReadLines=10&prefix="+prefix+"&source="+source;
 tests[j].n    = n || 50;
-tests[j].md5  = "cef9e50efae894c42293ed5922af2b8e";
+tests[j].md5  = "4dcc638a9d40bdf4f4c1c6d38ecfa979";
 
 j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=false&streamFilterReadLines=10&prefix="+prefix+"&source="+source;
 tests[j].n    = n || 50;
-tests[j].md5  = "cef9e50efae894c42293ed5922af2b8e";
+tests[j].md5  = "4dcc638a9d40bdf4f4c1c6d38ecfa979";
 
 j = j+1;
 tests[j]      = {};
@@ -95,7 +96,7 @@ j = j+1;
 tests[j]      = {};
 tests[j].url  = args + "&streamOrder=true&streamFilterReadLines=10&streamFilter=replace('2013','2013')&prefix="+prefix+"&source="+source;
 tests[j].n    = n || 50;
-tests[j].md5  = "cef9e50efae894c42293ed5922af2b8e";
+tests[j].md5  = "4dcc638a9d40bdf4f4c1c6d38ecfa979";
 
 j = j+1;
 tests[j]      = {};
