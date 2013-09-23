3b1b4cce1beba5fe56310941499ca55671423286
Robert Weigel <Robert.SWeigel@gmail.com>
2013-09-23 16:00:12 -0400

    New line formatter code.

--- plugins/formattedTime.js
+++ plugins/formattedTime.js
@@ -1,24 +1,46 @@
 var moment = require('moment');
 
-exports.extractLine = function (line, req) {
-		
-		var timeformat  = req.query.timeformat || req.body.timeformat || "";
-		var timecols    = req.query.timecols   || req.body.timecols || "";
+//if (1) {
+exports.extractSignature = function (options) {
+	var version = "1.0.0";
 
-		var timeformata = timeformat.split(" ");
-		var timecolsa   = timecols.split(",");
+	var xoptions = ""+options.req.query.timeformat+options.req.query.timecols+options.req.query.outformat;
+	//console.log(options.req.query.outformat)
+
+	xoptions = xoptions.replace(/undefined/g,"");
+	return version.split(".")[0] + xoptions;
+}
+
+exports.formatLine = function (line, options) {
+
+
+	var debug = false;
+	var timeformat = options.req.query.timeformat || "YYYY-MM-DDZHH:mm:ss.SSSZ";
+	var timecols   = options.req.query.timecols || 1;
+	var outformat  = options.req.query.outformat || 1;
+
+	var timeformata = timeformat.split(/,/);
+	var timecolsa   = timecols.split(/,/);
+
+	//console.log(timecolsa);
+	//console.log(parseInt(timecolsa[0])-1 + ", " + parseInt(timecolsa[timecolsa.length-1]))
+	//console.log(line.split(/\s+/))
+	// Assumes time is in continuous columns and before any data column that is to be kept.
+	timev      = line.split(/\s+/).slice(parseInt(timecolsa[0])-1,parseInt(timecolsa[timecolsa.length-1]));
+	datav      = line.split(/\s+/).slice(parseInt(timecolsa[timecolsa.length-1]));
+
+	if (debug) {
+		console.log("line: " + line);
+		console.log("timeformat array: " + timeformata.join(","));
+		console.log("time array: " + timev.join(","))
+		console.log("data array: " + datav.join(","));
+	}
+	d = moment(timev.join(" "),timeformat);
+	//console.log("date: ");console.log(d)
 	
-		// Assumes time is in continuous columns and before any data column that is to be kept.
-		timev      = line.split(" ").slice(parseInt(timecolsa[0])-1,parseInt(timecolsa[timecolsa.length-1]));
-		datav      = line.split(" ").slice(parseInt(timecolsa[timecolsa.length-1]));
-	
-		console.log(timeformata)
-		console.log(timev.join(" "))
-		console.log(datav);
-		d = moment(timev.join(" "),timeformat);
-		console.log(d)
-		
-		d = moment(timev.join(" "),timeformat)._a;
+	d = moment(timev.join(" "),timeformat)._a;
+
+	if (outformat == 1) {
 		d[1] = ((""+d[1]).length == 1) ? "0"+(d[1]+1) : (d[1]+1);
 		d[2] = ((""+d[2]).length == 1) ? "0"+d[2] : d[2];
 		d[3] = ((""+d[3]).length == 1) ? "0"+d[3] : d[3];
@@ -27,8 +49,18 @@ exports.extractLine = function (line, req) {
 		d[6] = ((""+d[6]).length == 1) ? "00"+d[6] : d[6];
 		d[6] = ((""+d[6]).length == 2) ? "0"+d[6] : d[6];
 	
-		console.log(d)
+		if (debug) console.log("Formatted date: " + d)
 		var timestamp = d[0]+"-"+d[1]+"-"+d[2]+"T"+d[3]+":"+d[4]+":"+d[5]+"."+d[6]+"Z";
 
-		return timestamp + " " + datav.join(" ");
+		if (debug) console.log("returning: "+timestamp + " " + datav.join(" "))
+		line = timestamp + " " + datav.join(" ");
+	}
+	if (outformat == 2) {
+		var timestamp = d[0]+" "+(parseInt(d[1])+1)+" "+d[2]+" "+d[3]+" "+d[4]+" "+d[5]+"."+d[6];
+		line = timestamp + " " + datav.join(" ");
+	}
+	
+	return line;
+
 }
+//}
