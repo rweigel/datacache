a53e6a8ab9c1c0e32c33f13026fa34fa6c2fa82c
Robert Weigel <rweigel@gmu.edu>
2014-08-18 22:10:22 -0400

    Clean-up of SuperMAG plug-in

--- plugins/supermag.js
+++ plugins/supermag.js
@@ -1,60 +1,51 @@
 exports.name = "supermag";
 
-exports.match = function(url){
-	return url.split("/")[2].toLowerCase()==="supermag.jhuapl.edu";
-}
-
-function pause(i) {
-	var date = new Date();
-	var curDate = null;
-	do { curDate = new Date(); }
-	while(curDate-date < i);
-}
-
-if (0) {
-exports.extractDataBinary = function (data) {
-
-    // Time stamp line
-    var re1 = /^([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)/;
-    
-    // Data line
-    var re2 = /\n([a-zA-Z]+)\s+([\d-]+)\s([\d-]+)\s([\d-]+)/g;
-    
-    // First combine timestamp and data line and remove station string.  Then split on newlines and keep data lines.
-	var arr = new Array();
-	var arr = data
-	    .toString()
-	    .replace(re2," $2 $3 $4")
-    	.replace(/[0-9](\n[A-Z])/g,'$1')  // Removes last element which is number of records that follow timestamp.
-	    .split("\n")
-	    .filter(function(line){return line.search(re1)!=-1;})
-	    .join("\n")
-	    .split(/\s+/g);
-
-	var buff = new Buffer(8*arr.length);
-	for (i = 0;i < arr.length; i++) {
-	    buff.writeDoubleLE(parseFloat(arr[i]),8*(i-1));
-	}
-	return buff;
-}
-}
+// Reads SuperMAG response and extracts data portion into format that is easy for TSDS2 + template to process.
 exports.extractData = function (data) {
 
 	// Time stamp line
-    var re1 = /^([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)/;
+	var re1 = /^([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)/;
 
-    // Magnetometer data line
-    var re2 = /\n([A-Z]+)\s+([\d-].*)/gi;
+	// Magnetometer data line
+	var re2 = /\n([A-Z]+)\s+([\d-].*)/gi;
 
-    // First combine timestamp and data line and remove station string.
-    // Then split on newlines, remove last line, and keep data lines.
-    
-    return data
-    		.toString()
-    		.replace(/[ \t]+/g,' ')			 // Replace tabs with space.
-    		.replace(/[0-9](\s\n[A-Z])/gi,'$1') // Removes last element of time lines which is number of records that follow timestamp.
-			.replace(re2,"$2")     // Removes newline and keeps data line.
+	// First combine timestamp and data line and remove station string.
+	// Then split on newlines, remove last line, and keep data lines.
+	
+	return data
+			.toString()
+			.replace(/[ \t]+/g,' ')				// Replace tabs with space.
+			.replace(/[0-9](\s\n[A-Z])/gi,'$1') // Removes last element of time lines which is number of records that follow timestamp.
+			.replace(re2,"$2")					// Removes newline and keeps data line.
 			.split("\n")
-			.filter(function (line) {return line.search(re1)!=-1}) // Removes lines that don't match re1.
+			.filter(function (line) {return line.search(re1)!=-1})	// Removes lines that don't match re1.
 			.join("\n") + "\n";
 };
+
+if (0) {
+	exports.extractDataBinary = function (data) {
+
+		// Time stamp line
+		var re1 = /^([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)\s+([\d]+)/;
+		
+		// Data line
+		var re2 = /\n([a-zA-Z]+)\s+([\d-]+)\s([\d-]+)\s([\d-]+)/g;
+		
+		// First combine timestamp and data line and remove station string.  Then split on newlines and keep data lines.
+		var arr = new Array();
+		var arr = data
+			.toString()
+			.replace(re2," $2 $3 $4")
+			.replace(/[0-9](\n[A-Z])/g,'$1')  // Removes last element which is number of records that follow timestamp.
+			.split("\n")
+			.filter(function(line){return line.search(re1)!=-1;})
+			.join("\n")
+			.split(/\s+/g);
+
+		var buff = new Buffer(8*arr.length);
+		for (i = 0;i < arr.length; i++) {
+			buff.writeDoubleLE(parseFloat(arr[i]),8*(i-1));
+		}
+		return buff;
+	}
+}
